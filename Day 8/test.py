5e8d5a743dd9c22e2edbcd10304a507b6d13853c
Edoardo <edo.allari@gmail.com>
2023-12-08 15:38:00 +0100

    Day 8 solution

--- Day 8/test.py
+++ Day 8/test.py
@@ -1,24 +1,9 @@
 instructions = "LRRLRRRLLRRRLRRRLLRRLRRRLRRLRRLRLRLRLRLRLLRRRLRRLRLRRRLRRRLRLRRRLRLRRLRRRLRRRLRLLRRRLRLLLRLRRRLRLRRLRRLLLLRRLRRLRLRLRRLRLRRLRRRLRRRLRLRLRRLLLLRRLRLRRLLRRRLRLRLRLRRRLRLLLRLRLRRRLRLRRRLRRRLRRRLLRRLRRRLRRRLRRRLRRRLRLLRRRLRLRRRLRLRLRRRLRRLRRLLRRRLRRRLRRRLRLRLRLRRLRRRLRRLRLRLRLRRRR"
 
 node_map = {}
+steps = 0
 current_keys = []
 final_keys = []
-total_steps = []
-
-
-def mcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
-def mcm(numeri):
-    risultato = numeri[0]
-    for num in numeri[1:]:
-        mcm_ab = risultato * num // mcd(risultato, num)
-        risultato = mcm_ab
-
-    return risultato
-    
 
 with open("input.txt") as file:
     for line in file:
@@ -30,17 +15,6 @@ for key in node_map.keys():
         current_keys.append(key)
     if key.endswith("Z"):
         final_keys.append(key)
-      
-for i in range(len(current_keys)):
-    current_key = current_keys[i]
-    steps = 0
-    while current_key not in final_keys:
-        if instructions[steps % len(instructions)] == "L":
-            current_key = node_map[current_key]["L"]
-        else:
-            current_key = node_map[current_key]["R"]
-        steps += 1
-    final_keys.remove(current_key)
-    total_steps.append(steps)
-
-print(mcm(total_steps))
+        
+print(current_keys)
+print(final_keys)     
