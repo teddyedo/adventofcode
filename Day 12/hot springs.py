f889dfea83f1304207177e31e7ba4169f47f31c7
Edoardo <edo.allari@gmail.com>
2023-12-13 00:59:56 +0100

    Day 12 part 1 solution

--- Day 12/hot springs.py
+++ Day 12/hot springs.py
@@ -1,30 +1,53 @@
-
-from itertools import permutations
-
+import re
 
 spring_map_combinations = 0
 
 def calculate_spring_combinations(line):
-    # line = line.strip().split()
-    spring_map = [x for x in line[0].split(".") if x != ""]
-    spring_occurrences = line[1].split(",")
-    # combs = combinations([".", "#"], line.count("?"))
-    # print(line)
-    # print(combs)
+    line = line.strip().split()
+    spring_map = line[0]
+    spring_occurrences = [int(i) for i in line[1].split(",")]
+    spring_combinations = get_all_spring_combinations(spring_map)
+    occ_matches = 0
+    regex = build_regex(spring_occurrences)
+    for combination in spring_combinations:
+        if sum(spring_occurrences) == combination.count("#") and  re.search(regex, combination):
+            occ_matches += 1
+    return occ_matches
+    
+def get_all_spring_combinations(spring_map):
+    
+    combs = []
+    unknown_springs = spring_map.count("?")
+    combs.append(spring_map)
+    for i in range(unknown_springs):
+        copy_combs = combs.copy()
+        for i in range(len(combs)):
+            combs[i] = combs[i].replace("?", "#", 1)
+        for j in range(len(copy_combs)):
+            copy_combs[j] = copy_combs[j].replace("?", ".", 1)
+        new_combs = []
+        for k in range(len(combs)):
+            new_combs.append(combs[k])
+            new_combs.append(copy_combs[k])
+        combs = new_combs
+        
+    return combs
 
-
-    return 1
+def build_regex(spring_occurencies):
+    regex = r"\.*"
+    for i in range(len(spring_occurencies)):
+        if i + 1 != len(spring_occurencies):
+            regex += "#" * int(spring_occurencies[i])
+            regex += r"\.+"
+        else:
+            regex += "#" * int(spring_occurencies[i])
+    regex += r"\.*"
+    
+    return regex
+    
 
 with open("input.txt") as file:
     for line in file:
         spring_map_combinations += calculate_spring_combinations(line)
     print(spring_map_combinations)
-        
-
-
-elements = [1, 2, 3, 4]
-
-    # Iterate through combinations of length 2
-for combo in permutations(elements, 3):
-    print(combo)
-    
+            
