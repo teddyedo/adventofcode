d85a126bff785029f004740fa948c61a942fdbd5
Edoardo <edo.allari@gmail.com>
2023-12-21 20:13:48 +0100

    Day 16 part 1 solution

--- Day 16/the floor will be lava.py
+++ Day 16/the floor will be lava.py
@@ -8,49 +8,53 @@ def calculate_next_step(coords, direction):
     elif direction == "DOWN":
         return (coords[0], coords[1] + 1)
 
-def is_valid_step(step):
-    return step[0] >= 0 and step[1] >= 0 and step[0] < len(contraption[0]) and step[1] < len(contraption)
+def is_valid_step(step, direction):
+    return step[0] >= 0 and step[1] >= 0 and step[0] < len(contraption[0]) and step[1] < len(contraption) and (step, direction) not in move_made
 
 def make_a_step(coords, direction):
     next_step = calculate_next_step(coords, direction)
-    print(coords, direction)
-    if is_valid_step(next_step):
+    if is_valid_step(next_step, direction):
         light_map[next_step[1]][next_step[0]] = "#"
-        if contraption[next_step[1]][next_step[0]] == "/":
-            if direction == "UP":
-                make_a_step(next_step, "RIGHT")
-            elif direction == "RIGHT":
-                make_a_step(next_step, "UP")
-            elif direction == "DOWN":
-                make_a_step(next_step, "LEFT")
-            elif direction == "LEFT":
-                make_a_step(next_step, "DOWN")
-        elif contraption[next_step[1]][next_step[0]] == "\\":
-            if direction == "UP":
-                make_a_step(next_step, "LEFT")
-            elif direction == "LEFT":
-                make_a_step(next_step, "UP")
-            elif direction == "DOWN":
-                make_a_step(next_step, "RIGHT")
-            elif direction == "RIGHT":
-                make_a_step(next_step, "DOWN")
-        elif contraption[next_step[1]][next_step[0]] == "|":
-            if direction == "LEFT" or direction == "RIGHT":
-                make_a_step(next_step, "UP")
-                make_a_step(next_step, "DOWN")
-            else:
-                make_a_step(next_step, direction)
-        elif contraption[next_step[1]][next_step[0]] == "-":
-            if direction == "UP" or direction == "DOWN":
-                make_a_step(next_step, "LEFT")
-                make_a_step(next_step, "RIGHT")
-            else:
-                make_a_step(next_step, direction)
+        move_made.append((next_step, direction)) # (step, direction)
+        if contraption[next_step[1]][next_step[0]] in "\\/|-":
+            if contraption[next_step[1]][next_step[0]] == "/":
+                if direction == "UP":
+                    make_a_step(next_step, "RIGHT")
+                elif direction == "RIGHT":
+                    make_a_step(next_step, "UP")
+                elif direction == "DOWN":
+                    make_a_step(next_step, "LEFT")
+                elif direction == "LEFT":
+                    make_a_step(next_step, "DOWN")
+            elif contraption[next_step[1]][next_step[0]] == "\\":
+                if direction == "UP":
+                    make_a_step(next_step, "LEFT")
+                elif direction == "LEFT":
+                    make_a_step(next_step, "UP")
+                elif direction == "DOWN":
+                    make_a_step(next_step, "RIGHT")
+                elif direction == "RIGHT":
+                    make_a_step(next_step, "DOWN")
+            elif contraption[next_step[1]][next_step[0]] == "|":
+                if direction == "LEFT" or direction == "RIGHT":
+                    make_a_step(next_step, "UP")
+                    make_a_step(next_step, "DOWN")
+                else:
+                    make_a_step(next_step, direction)
+            elif contraption[next_step[1]][next_step[0]] == "-":
+                if direction == "UP" or direction == "DOWN":
+                    make_a_step(next_step, "LEFT")
+                    make_a_step(next_step, "RIGHT")
+                else:
+                    make_a_step(next_step, direction)
         else:
-            make_a_step(next_step, direction)   
+            make_a_step(next_step, direction)
+
 
 contraption = []
 light_map = []
+move_made = []
+energized_tiles = 0
 
 with open("input.txt") as file:
     for row in file:
@@ -60,8 +64,8 @@ light_map = [["." for _ in range(len(contraption[0]))] for _ in range(len(contra
 
 start_coords = (-1, 0)
 
-
 make_a_step(start_coords, "RIGHT")
 
 for row in light_map:
-    print(row)
+    energized_tiles += row.count("#")
+print(energized_tiles)
